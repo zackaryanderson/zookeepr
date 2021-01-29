5b0d390a68ca58e1eda21ef0871e79c5b9220419
zanderson1998 <zanderson1998@yahoo.com>
2021-01-29 16:52:52 -0700

    add zookeeper data and routing

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalRoutes');
+
+
+router.use(animalRoutes);
+router.use(require('./zookeeperRoutes'));
+
+
+module.exports = router;
---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const path = require('path');
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+
+router.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+});
+
+module.exports = router;
